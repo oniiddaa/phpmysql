a59fd1374d01550d4925056a3905a2e7737cecbd
oniiddaa <onidamisiri1112@gmail.com>
2026-09-15 19:13:44 +0200

    modul7

---
+++ lesson 7/CreateDB_PDO.php
@@ -0,0 +1,16 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+
+    try{
+        $conn = new PDO("mysql:host=$host",$user,$pass);
+        $sql = "Create database testdb";
+        $conn->exec($sql);
+        echo "Database is created";
+    }catch(Exception $e){
+        echo "Database not created, something went wrong!";
+    }
+
+?>
